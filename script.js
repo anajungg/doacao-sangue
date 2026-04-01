7e81c74b3a6fd8b444a188a3537c8a94fc2328ad
anajungg <ana_l_jung@estudante.sesisenai.org.br>
2026-04-01 09:45:10 -0300

    add-relizei as principais funcionalidades do formulario

---
+++ script.js
@@ -0,0 +1,82 @@
+let form = document.getElementById('formulario');
+
+form.addEventListener('submit', function (e) {
+
+    e.preventDefault();
+
+    let valido = true;
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let idade = document.getElementById('idade').value;
+    let cidade = document.getElementById('cidade').value;
+    let sangue = document.getElementById('sangue').value;
+    let peso = document.getElementById('peso').value;
+    let estado = document.getElementById('estado').value;
+    
+
+    document.getElementById('erroNome').textContent = '';
+    document.getElementById('erroEmail').textContent = '';
+
+if (nome.length < 7) {
+    document.getElementById('erroNome').textContent = 'Nome deve ter pelo menos nome e sobrenome';
+    valido = false;
+}
+
+if (!email.includes('@')) {
+document.getElementById('erroEmail').textContent = 'Email Inválido, deve conter @.';
+valido = false;
+}
+if (telefone.length < 8) {
+alert("Telefone inválido.");
+valido = false;
+}
+if (telefone === "") {
+    alert("Telefone é obrigatória.");
+    valido = false;
+}
+if (idade === "" || idade <= 0) {
+    alert("Informe uma idade válida.");
+    valido = false;
+}
+if (idade >= 16 ) {
+    alert("Você precisa ter 16 anos ou mais para doar sangue.");
+    valido = false;
+}
+if (cidade === "") {
+    alert("Cidade é obrigatória.");
+    valido = false;
+}
+if (estado === "") {
+    alert("Estado é obrigatória.");
+    valido = false;
+}
+if (peso < 50) {
+    alert("Você deve ter um peso mínimo de 50kg. ");
+    valido = false;
+}
+if (peso === "") {
+    alert("Peso é obrigatório.");
+    valido = false;
+}
+if (sangue === "selecione") {
+    alert("Você precisa selecionar seu tipo sanguíneo.");
+    valido = false;
+}
+if (valido) {
+    let resultado = document.getElementById('resultado');
+
+    resultado.innerHTML = `
+    Dados enviados:<br>
+    Nome: ${nome} <br>
+    Email: ${email} <br>
+    Telefone: ${telefone} <br>
+    CPF: ${cpf} <br>
+    Idade: ${idade} <br>
+    Cidade: ${cidade} <br>
+    `;
+
+    form.reset();
+}
+});
